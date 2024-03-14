c00f1d1f41aa1797f6e7922df75ac658b2a0c1d5
Badmus Damola Khadijah <Damolabadmus014@gmail.com>
2024-03-14 20:40:59 +0000

    Add fixed scroll button

---
+++ assets/icons/ArrowUpIcon.vue
@@ -0,0 +1,18 @@
+<template>
+    <svg
+  width="20"
+  height="20"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M19 10L12 3M12 3L5 10M12 3L12 21"
+    stroke="#55566F"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</svg>
+
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,50 @@
+<script setup lang="ts">
+import ArrowUp from "@/assets/icons/ArrowUpIcon";
+
+const show = ref(false);
+
+const handleScroll = () => {
+  show.value = window.scrollY > 100;
+};
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
+</script>
+
 <template>
-    <main class="custom-max-width overflow-hidden overscroll-x-none  ">
-      <hero-section />
-      <about-me class=" "/>
-      <my-skills/>
-      <my-work/>
-      <contact/>
-    </main>
-  </template>
-  <script setup></script>
+  <main class="custom-max-width overflow-hidden overscroll-x-none">
+    <hero-section />
+    <about-me class=" " />
+    <my-skills />
+    <my-work />
+    <contact />
+    <div v-if="show" @click="scrollToTop" class="fixed-arrow">
+      <arrow-up />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.fixed-arrow {
+  position: fixed;
+  bottom: 20px;
+  right: 10px;
+  background-color: #ffc1c9;
+  color: white;
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 50%;
+  z-index: 99999;
+}
+</style>
