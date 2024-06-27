ffa598cdc09f459d266f40d79a92138492c86f15
Badmus Damola Khadijah <Damolabadmus014@gmail.com>
2024-06-27 21:29:50 +0100

    optimize fonts

--- components/base/Header.vue
+++ components/base/Header.vue
@@ -60,7 +60,7 @@ const toggleSideNav = () => {
       <div v-if="showSidenav" class="lg:hidden">
         <side-nav @close-side-nav="toggleSideNav" />
       </div>
-      <button @click="toggleTheme" class=" w-[41px] h-[41px] flex items-center">
+      <button @click="toggleTheme" class=" w-[41px] h-[41px] flex items-center" aria-label="Toggle theme">
         <dark-icon v-show="colorMode.preference === 'light'" />
         <light-icon v-show="colorMode.preference === 'dark'" />
       </button>
