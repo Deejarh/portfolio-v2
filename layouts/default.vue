ca40884de89467a07b6a1edea3d977dbf16820f0
Badmus Damola Khadijah <Damolabadmus014@gmail.com>
2024-07-19 16:18:54 +0100

    Add analtyics

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+import { Analytics } from '@vercel/analytics/react';
 const appDescription = "Badmus Damola Taiwo is a highly proficient frontend developer with over three years of invaluable experience in the fintech/credit Industry, consistently delivering exceptional user experiences for a range of web applications"
 
 
@@ -20,6 +21,7 @@ useHead({
       <base-header class="fixed top-4 left-0 right-0 m-auto  z-50" />
       <slot/>
       <base-footer />
+      <Analytics />
       <SpeedInsights />
     </div>
   </template>
